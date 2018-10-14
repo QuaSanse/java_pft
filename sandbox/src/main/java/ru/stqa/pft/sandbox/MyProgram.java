fce5cb8ad66e0c56ae1db6f10d7a3aa0ac30247e
Дима Шумихин <quasanse@gmail.com>
2018-10-14 18:25:47 +0300

    Создана функция distance 1 вариант

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -7,10 +7,18 @@ public class MyProgram {
 		hello ("world");
 		hello ("user");
 		hello ("Dima");
+
+		double p1 = 2;
+		double p2 = 4;
+		System.out.println("Расстояние между точками p1 и p2 = " + distance (p1, p2));
 	}
 
 	public static void hello (String h) {
 		System.out.println("Hello," + h + "!");
+	}
+
+	public static double distance(double p1, double p2) {
+		return p2 - p1;
 	}
 
 }
