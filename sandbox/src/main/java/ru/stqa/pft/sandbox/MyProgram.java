f04e015882bf529c64899c059bf1595fa742d096
Дима Шумихин <quasanse@gmail.com>
2018-10-17 21:50:46 +0300

    Исправил расчет функции distance №3 вариант добавлена переменная d

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -11,13 +11,13 @@ public class MyProgram {
 
 		Point p1 = new Point(2, 4);
 		Point p2 = new Point(6, 8);
-		double d = Math.sqrt (p2.x - p1.x) + (p2.y - p1.y);
+		double distance = Math.sqrt (((p2.x - p1.x)* (p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
 		//p1.x = 2;
 		//p1.y = 4;
 		//p2.x = 6;
 		//p2.y = 8;
 
-			System.out.println("Расстояние между точками p1 и p2 = " + d);
+			System.out.println("Расстояние между точками p1 и p2 = " + distance);
 	}
 
 	public static void hello (String h) {
