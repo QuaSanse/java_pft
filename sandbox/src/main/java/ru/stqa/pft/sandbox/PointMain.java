f02e609407e746f9336ef7e77c9dd799ace4cc31
Дима Шумихин <quasanse@gmail.com>
2018-10-18 19:45:27 +0300

    расчет функции distance №5 вариант

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointMain.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointMain.java
@@ -7,6 +7,8 @@ public class PointMain {
 
     System.out.println ("Расстояние между точками p1 и p2 = " + distance (p1,p2));
 
+    System.out.println("Расстояние между точками p1 и p2 = " + p1.distance(p2));
+
   }
   public static double distance(Point p1, Point p2) {
     return Math.sqrt (((p2.x - p1.x)* (p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
