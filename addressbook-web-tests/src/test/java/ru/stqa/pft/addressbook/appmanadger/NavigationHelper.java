394ad1ee283e0a0832087213f9b19fa5b6321976
Дима Шумихин <quasanse@gmail.com>
2018-10-21 20:39:45 +0300

    Group Создаём общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanadger/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanadger/NavigationHelper.java
@@ -3,15 +3,13 @@ package ru.stqa.pft.addressbook.appmanadger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-
-  FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoCroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
