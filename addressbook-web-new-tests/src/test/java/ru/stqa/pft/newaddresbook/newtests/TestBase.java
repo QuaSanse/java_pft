30297584ab4f14c2bb06197541b292036486c15e
Дима Шумихин <quasanse@gmail.com>
2018-10-26 21:26:11 +0300

    Задание №7 Добавлен запуск тестов в разных браузерах

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/TestBase.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.newaddresbook.newtests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.newaddresbook.newappmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager appl = new ApplicationManager();
+  protected final ApplicationManager appl = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
