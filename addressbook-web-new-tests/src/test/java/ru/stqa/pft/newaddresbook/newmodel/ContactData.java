a1918ee9427b2a9f0236ed7348cdebb268266cae
Дима Шумихин <quasanse@gmail.com>
2018-11-07 22:18:01 +0300

    Задание №9 Добавлена сортировка для добавления и изменения групп и контактов

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newmodel/ContactData.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newmodel/ContactData.java
@@ -25,7 +25,7 @@ public class ContactData {
           String lastname,
           String adress,
           String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.adress = adress;
@@ -73,7 +73,6 @@ public class ContactData {
 
     ContactData that = (ContactData) o;
 
-    if (id != that.id) return false;
     if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
     if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
     return adress != null ? adress.equals(that.adress) : that.adress == null;
@@ -81,8 +80,7 @@ public class ContactData {
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    int result = firstname != null ? firstname.hashCode() : 0;
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
     result = 31 * result + (adress != null ? adress.hashCode() : 0);
     return result;
--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/ContactModificationTests.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/ContactModificationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.newaddresbook.newtests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.newaddresbook.newmodel.ContactData;
+import ru.stqa.pft.newaddresbook.newmodel.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -37,6 +39,11 @@ public class ContactModificationTests extends TestBase {
 
     before.remove(before.size() - 1);
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+    //Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
