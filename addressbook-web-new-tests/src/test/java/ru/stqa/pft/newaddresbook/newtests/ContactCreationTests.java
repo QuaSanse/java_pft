0abd2446e7f7052af20904199d3b86b2d472049d
Дима Шумихин <quasanse@gmail.com>
2018-11-18 18:18:34 +0300

    Лекция 6.5 Добавлена загрузка из файла для групп и контактов

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/ContactCreationTests.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/ContactCreationTests.java
@@ -4,9 +4,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.newaddresbook.newmodel.ContactData;
 import ru.stqa.pft.newaddresbook.newmodel.Contacts;
-import ru.stqa.pft.newaddresbook.newmodel.GroupData;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,23 +15,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
   @DataProvider
-  public Iterator<Object[]> validContact() {
+  public Iterator<Object[]> validContact() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
     File photo = new File("src/test/resources/stru.png");
-    list.add(new Object[] {new ContactData()
-            .withFirstname("Firstname")
-            .withLastname("lastname")
-            .withPhoto(photo)
-            .withAddress("address")
-            .withGroup("Group")
-    });
-    list.add(new Object[] {new ContactData()
-            .withFirstname("Firstname2")
-            .withLastname("lastname2")
-            .withPhoto(photo)
-            .withAddress("address2")
-            .withGroup("Group")
-    });
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData()
+              .withFirstname(split[0])
+              .withLastname(split[1])
+              .withAddress(split[2])
+              .withPhoto(photo)
+      });
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
