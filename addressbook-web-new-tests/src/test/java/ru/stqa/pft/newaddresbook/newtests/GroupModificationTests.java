852b28073eef384488d686a367a300826d2c2024
Дима Шумихин <quasanse@gmail.com>
2018-11-25 20:10:10 +0300

    Добавлено сравнение из БД для модификации групп

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/GroupModificationTests.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/GroupModificationTests.java
@@ -12,24 +12,25 @@ public class GroupModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
       app.group().create(new GroupData().withName("test1"));
     }
   }
 
   @Test
   public void testModificationGroup() {
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData modifieddGroup = before.iterator().next();
     GroupData group = new GroupData()
             .withId(modifieddGroup.getId())
             .withName("test1")
             .withHeader("groupheader")
             .withFooter("groupfooter");
+    app.goTo().groupPage();
     app.group().modify(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifieddGroup).withAdded(group)));
   }
 }
