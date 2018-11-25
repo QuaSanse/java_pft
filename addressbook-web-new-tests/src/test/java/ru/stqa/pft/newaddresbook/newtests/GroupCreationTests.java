83467155617f8453a36e37d2effbd2363f14844f
Дима Шумихин <quasanse@gmail.com>
2018-11-25 20:21:36 +0300

    Добавлено сравнение из БД для создания и удаления групп

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/GroupCreationTests.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/GroupCreationTests.java
@@ -59,11 +59,11 @@ public class GroupCreationTests extends TestBase {
   @Test(dataProvider = "validGroupsFromJson")
   public void testCreationNewGroup(GroupData group) {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     //GroupData group = new GroupData().withName("test1");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
