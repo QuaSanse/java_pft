71b6d87819a277c158b1ab23aa63585687b4ca84
Дима Шумихин <quasanse@gmail.com>
2018-11-29 21:59:12 +0300

    Добавлен файл AddContactInGroup Черновик 2

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/AddContactInGroup.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/AddContactInGroup.java
@@ -3,19 +3,37 @@ package ru.stqa.pft.newaddresbook.newtests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+import ru.stqa.pft.newaddresbook.newmodel.ContactData;
 
 public class AddContactInGroup extends TestBase {
   WebDriver wd;
 
   @Test
-  public void testsAddContactInGroup() {
-    @Test
-    public void testAddContactInGroup() {
-      wd.findElement(By.id("151")).click();
-      wd.findElement(By.name("to_group")).click();
-      wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Select all'])[1]/following::option[1]")).click();
-      wd.findElement(By.name("add")).click();
-      wd.findElement(By.linkText("group page \"test1\"")).click();
-    }
+  public void testAddContactInGroup() {
+    app.goTo().contactPage();
+    ContactData contact = new ContactData();
+    //выбор контакта
+    app.contact().selectContactDeletionById(contact.getId());
+    //wd.findElement(By.id("151")).click();
+    //раскрытие списка групп
+    selectToGroup();
+    //выбор из списка группу
+    selectedNameGroup();
+    //клик по кнопке добавления
+    inputAdd();
+    //переход по ссылке
+    wd.findElement(By.linkText("group page \"test1\"")).click();
+  }
+
+
+
+  private void selectToGroup() {
+    wd.findElement(By.name("to_group")).click();
+  }
+  private void selectedNameGroup() {
+    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Select all'])[1]/following::option[1]")).click();
+  }
+  private void inputAdd() {
+    wd.findElement(By.name("add")).click();
   }
 }
