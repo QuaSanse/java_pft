f408f63c4aca3e7b711e1502d8fbc23768d57f0a
Дима Шумихин <quasanse@gmail.com>
2018-12-02 19:30:10 +0300

    Задание №16: Реализован тест для добавления контактов в группу с предусловиями

--- addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/AddContactInGroup.java
+++ addressbook-web-new-tests/src/test/java/ru/stqa/pft/newaddresbook/newtests/AddContactInGroup.java
@@ -1,11 +1,31 @@
 package ru.stqa.pft.newaddresbook.newtests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.newaddresbook.newmodel.ContactData;
 import ru.stqa.pft.newaddresbook.newmodel.Contacts;
+import ru.stqa.pft.newaddresbook.newmodel.GroupData;
 
 
 public class AddContactInGroup extends TestBase {
+  @BeforeMethod
+  public void ensurePreconditions() {
+    //Добавляем группу
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test1"));
+    }
+    //Добавляем контакт без группы
+    if (app.db().contacts().size() == 0) {
+      app.goTo().contactPage();
+      app.contact().create(new ContactData()
+                      .withFirstname("Firstname")
+                      .withLastname("lastname")
+                      .withAddress("adress")
+              //.withGroup("Group")
+              , true);
+    }
+  }
 
   @Test
   public void testAddContactInGroup() {
@@ -23,6 +43,7 @@ public class AddContactInGroup extends TestBase {
     //клик по кнопке добавления
     app.contact().inputAdd();
     //переход по ссылке
+    app.contact().returnToHomePage();
     //wd.findElement(By.linkText("group page \"test1\"")).click();
   }
 }
