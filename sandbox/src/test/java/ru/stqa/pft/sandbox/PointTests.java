26906ad2bfa70c45b308267abd098ecf1447981c
Дима Шумихин <quasanse@gmail.com>
2018-12-16 17:27:14 +0300

    Закомментированы плохие тесты

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -11,12 +11,12 @@ public class PointTests {
     assert p1.distance(p2) == 5.656854249492381;
   }
 
-  //@Test
+  /*@Test
   public void testDistanceN2 () {
     Point p1 = new Point(2, 4);
     Point p2 = new Point(6, 8);
     assert p1.distance(p2) == 5;
-  }
+  }*/
 
   @Test
   public void testDistanceN3 () {
@@ -25,11 +25,11 @@ public class PointTests {
     Assert.assertEquals(P1.distance(P2), 5.656854249492381);
   }
 
-  //@Test
+  /*@Test
   public void testDistanceN4 () {
     Point P1 = new Point(2, 4);
     Point P2 = new Point(6, 8);
     Assert.assertEquals(P1.distance(P2), 5);
-  }
+  }*/
 
 }
